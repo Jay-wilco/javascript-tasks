862cf4f282ab2031a3cce91f72071ff80f403559
Jay-wilco <jtvilkko@gmail.com>
2025-03-15 23:40:51 +0200

    add modal, font and some colors

--- Tasks/04_week/Pancake/script.js
+++ Tasks/04_week/Pancake/script.js
@@ -13,6 +13,35 @@ const customerName = document.querySelector("#customerName");
 const delivery = document.querySelectorAll(".delivery");
 // const selectedDelivery = document.querySelector(".delivery:checked");
 
+// Modal orderlist ??
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+makeOrderBtn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+/*End of modal maddness? */
+
 let selectedToppings = [];
 let selectedExtras = [];
 let selectedDelivery = []; // Initialize as an empty array
@@ -133,11 +162,11 @@ makeOrderBtn.addEventListener("click", () => {
     status: "waiting",
   };
 
-  let orders = JSON.parse(localStorage.getItem("getOrders")) || [];
+  let orders = JSON.parse(localStorage.getItem("orders")) || [];
   orders.push(newOrder);
-  localStorage.setItem("getOrders", JSON.stringify(orders));
+  localStorage.setItem("orders", JSON.stringify(orders));
 
-  alert("Thank you, your order has been sent to the Chef!");
+  // alert("Thank you, your order has been sent to the Chef!");
 
   summaryText.textContent = "";
   pancakeForm.reset();
